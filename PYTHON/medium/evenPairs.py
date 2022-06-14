abba06ced7f77c0224d701dd62ee13f928c8686e
Mustafa Bilal Dogan <mbilaldogan90@gmail.com>
2022-06-14 14:45:33 +0300

    even pairs

---
+++ PYTHON/medium/evenPairs.py
@@ -0,0 +1,30 @@
+""" 
+Even Pairs
+Have the function EvenPairs(str) take the str parameter being passed and determine if a pair of adjacent even numbers exists anywhere in the string. If a pair exists, return the string true, otherwise return false. For example: if str is "f178svg3k19k46" then there are two even numbers at the end of the string, "46" so your program should return the string true. Another example: if str is "7r5gg812" then the pair is "812" (8 and 12) so your program should return the string true.
+
+Examples
+
+Input: "3gy41d216"
+Output: true
+
+Input: "f09r27i8e67"
+Output: false
+
+"""
+def EvenPairs(strParam):
+
+  res = False 
+
+  for i in strParam :
+    if i in "02468" :
+      if res :
+        return "true"
+      res = True
+    elif i.isalpha():
+      res = False
+  return "false"
+  
+
+# keep this function call here 
+EvenPairs("f09r27i8e67")
+
