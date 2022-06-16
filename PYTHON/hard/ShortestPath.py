675e48a2d98126b5c0639ae0868f34661f0fa52a
Mustafa Bilal Dogan <mbilaldogan90@gmail.com>
2022-06-16 21:27:01 +0300

    solution 2

--- PYTHON/hard/ShortestPath.py
+++ PYTHON/hard/ShortestPath.py
@@ -73,3 +73,42 @@ def ShortestPath(strArr):
     return path
 #keep this function call here
 ShortestPath(["5","A","B","C","D","F","A-B","A-C","B-C","C-D","D-F"])	
+
+""" solution 2 """
+
+def ShortestPath(strArr):
+    street_num = int(strArr[0])
+    streets = strArr[1:street_num+1]
+    paths = strArr[street_num+1:]
+    possible_paths = {}
+    start = streets[0]
+    finish = streets[-1]
+
+    for street in streets:
+        connections = []
+        for path in paths:
+            if street in path:
+                street1, street2 = tuple(path.split('-'))
+                
+                if street == street1:
+                    connections.append(street2)
+                    possible_paths[street] = connections
+
+                else:
+                    connections.append(street1)
+                    possible_paths[street] = connections
+
+    all_roads = [start]
+    road = start
+    for x in all_roads:
+        for next_street in possible_paths[x[-1]]:
+            road = x + next_street
+            if road.count(next_street) > 1:
+                continue
+            
+            all_roads.append(road)
+            
+            if next_street == finish:
+                return '-'.join(road)
+
+    return -1
