e18a78f65e6fb9151e746a9d82fe3774a30fe360
Mustafa Bilal Dogan <mbilaldogan90@gmail.com>
2022-06-30 05:55:41 +0300

    DashInsert

---
+++ JS/easy/DashInsert.js
@@ -0,0 +1,31 @@
+/* 
+Dash Insert
+Have the function DashInsert(str) insert dashes ('-') between each two odd numbers in str. For example: if str is 454793 the output should be 4547-9-3. Don't count zero as an odd number.
+
+Examples
+
+Input: 99946
+Output: 9-9-946
+
+Input: 56730
+Output: 567-30
+
+*/
+
+function DashInsert(str) { 
+
+    str = str.toString() ;
+    let result = "" 
+  
+    for(i = 0 ; i < str.length ; i++) {
+      if(i > 0 && str[i] % 2 === 1 && str[i - 1] % 2 === 1) {
+        result += "-"
+      }
+    result += str[i]
+  }
+  return result 
+  }
+  
+     
+  // keep this function call here 
+DashInsert(99946);
