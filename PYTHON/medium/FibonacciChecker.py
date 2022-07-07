881a193ebba83b5c8b773a4425bd1815d84efb92
Mustafa Bilal Dogan <mbilaldogan90@gmail.com>
2022-07-07 16:22:36 +0300

    Fibonacci Checker

---
+++ PYTHON/medium/FibonacciChecker.py
@@ -0,0 +1,24 @@
+""" 
+Fibonacci Checker
+Have the function FibonacciChecker(num) return the string yes if the number given is part of the Fibonacci sequence. This sequence is defined by: Fn = Fn-1 + Fn-2, which means to find Fn you add the previous two numbers up. The first two numbers are 0 and 1, then comes 1, 2, 3, 5 etc. If num is not in the Fibonacci sequence, return the string no.
+Examples
+Input: 34
+Output: yes
+Input: 54
+Output: no
+
+ """
+
+def FibonacciChecker(num):
+  number = numberOne = 0
+  numberTwo = 1
+
+  while number <= num :
+    if number == num :
+      return "yes"
+    number = numberOne +numberTwo
+    numberTwo = numberOne
+    numberOne = number
+  return "no"
+# keep this function call here 
+FibonacciChecker(34)
